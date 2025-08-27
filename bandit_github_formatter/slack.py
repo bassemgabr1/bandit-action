278507d2bfaa22d553f60589ff2c9db2224b196e
Bassem Sadaqah <bassem.gabr@thndr.app>
2025-08-27 15:32:05 +0300

    fix slack token parsing

--- bandit_github_formatter/slack.py
+++ bandit_github_formatter/slack.py
@@ -2,8 +2,8 @@ from slack_sdk import WebClient
 import os
 # Sends message to Slack channel with given message
 def send_message(message: str, SLACK_CHANNEL: str) -> dict:
-    SLACK_TOKEN = os.getenv("SLACK_TOKEN", "none")
-    SLACK_USERNAME = os.getenv("SLACK_USERNAME", "bandit-test")
+    SLACK_TOKEN = os.getenv("INPUT_SLACK_TOKEN", "none")
+    SLACK_USERNAME = os.getenv("INPUT_SLACK_USERNAME", "bandit-test")
     client = WebClient(token=SLACK_TOKEN)
     client.chat_postMessage(
         channel=SLACK_CHANNEL, 
