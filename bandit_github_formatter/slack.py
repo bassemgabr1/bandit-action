42f90af4b5a41d8349d4bd528b0ec785c02a2155
Bassem Sadaqah <bassem.gabr@thndr.app>
2025-08-27 15:30:15 +0300

    fix slack send_message

--- bandit_github_formatter/slack.py
+++ bandit_github_formatter/slack.py
@@ -1,12 +1,12 @@
 from slack_sdk import WebClient
 import os
 # Sends message to Slack channel with given message
-async def send_message(message: dict, SLACK_CHANNEL: str) -> dict:
+def send_message(message: str, SLACK_CHANNEL: str) -> dict:
     SLACK_TOKEN = os.getenv("SLACK_TOKEN", "none")
     SLACK_USERNAME = os.getenv("SLACK_USERNAME", "bandit-test")
     client = WebClient(token=SLACK_TOKEN)
     client.chat_postMessage(
         channel=SLACK_CHANNEL, 
-        blocks=message, 
+        text=message, 
         username=SLACK_USERNAME
     )
